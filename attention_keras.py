c8e9edc9fdcf82460a6bf68dd2da5cad73db3f15
陈耀 <chenyao@chenyaodeMacBook-Pro.local>
2020-07-21 14:36:44 +0800

    0721

--- attention_keras.py
+++ attention_keras.py
@@ -4,13 +4,25 @@ from keras.layers import *
 import keras.backend as K
 
 
-def to_mask(x, mask, model='mul'):
+def to_mask(x, mask, mode='mul'):
+    if mask is None:
+        return x
+    else:
+        for _ in range(K.ndim(x) - K.ndim(mask)):
+            mask = K.expand_dims(mask, K.ndim(mask))
+        if mode == 'mul':
+            return x * mask
+        else:
+            return x - (1 - mask) * 1e10
+
+
+def extract_seq_patches(x, kernel_size, rate):
     pass
 
 
 class OurLayer(Layer):
     """
-     增加reuse方法，允许在定义layer时调用现成的层
+     增加reuse方法，允许在定义layer时调用现成的层，这样可以重复使用。不然call过了以后不能复用
      """
 
     def reuse(self, layer, *args, **kwargs):
@@ -60,3 +72,37 @@ class Attention(OurLayer):
     def call(self, inputs):
         q, k, v = inputs[:3]
         v_mask, q_mask = None, None
+        # mask.shape=[batch_size, seq_len] or [batch_size, seq_len, 1]
+        if len(inputs) > 3:
+            v_mask = inputs[3]
+            if len(inputs) > 4:
+                q_mask = inputs[4]
+
+        # 线性变换
+        qw = self.reuse(self.q_dense, q)
+        kw = self.reuse(self.k_dense, k)
+        vw = self.reuse(self.v_dense, v)
+
+        # 形状变换
+        qw = K.reshape(qw, (-1, K.shape(qw)[1], self.heads, self.key_size))
+        kw = K.reshape(kw, (-1, K.shape(kw)[1], self.heads, self.key_size))
+        vw = K.reshape(vw, (-1, K.shape(vw)[1], self.heads, self.size_per_head))
+
+        # 维度置换
+        qw = K.permute_dimensions(qw, [0, 2, 1, 3])
+        kw = K.permute_dimensions(kw, [0, 2, 1, 3])
+        vw = K.permute_dimensions(vw, [0, 2, 1, 3])
+
+        # Attention
+        a = K.batch_dot(qw, kw, [3, 3]) / self.key_size ** 0.5
+        a = K.permute_dimensions(a, [0, 3, 2, 1])
+        a = to_mask(a, v_mask, 'add')
+        a = K.permute_dimensions(a, (0, 3, 2, 1))
+        if (self.mask_right is not False) or (self.mask_right is not None):
+            if self.mask_right is True:
+                ones = K.ones_like(a[: 1, : 1])
+                mask = (ones - K.tf.matrix_band_part(ones, -1, 0)) * 1e10
+                a = a - mask
+            else:
+                pass
+
